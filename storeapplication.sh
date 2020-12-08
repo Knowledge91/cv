d0d6b48ff76a4aec56f020c84fbec74bb178af9e
Dr. Dirk Hornung <hello@drdirk.io>
2020-12-08 09:18:53 +0100

    Applied for Senior developer at Glencore.

--- storeapplication.sh
+++ storeapplication.sh
@@ -1,4 +1,4 @@
 VERSION=$(ls -lv ./versions | tail -n 1 | awk '{print $9}' | sed 's/[^0-9]*//g')
 VERSION=$(($VERSION + 1))
-cp cv.md ./versions/${VERSION}.md
-cp resume.pdf ./versions/${VERSION}.pdf
+cp cv.md ./versions/v${VERSION}.md
+cp resume.pdf ./versions/v${VERSION}.pdf
